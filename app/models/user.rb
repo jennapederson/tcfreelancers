957047ffe3ec089cba600c4a7137632f98194418
Jenna Pederson <jenna@612softwarefoundry.com>
2015-01-19 19:46:10 +0000

    Initial stab at getting real data on the home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,21 +17,5 @@ class User < ActiveRecord::Base
       user.profile_url = auth.info.urls.public_profile
       user.profile_image = auth.info.image
     end
-
-    # user = where(provider: auth.provider, uid: auth.uid).first
-    # if user.nil?
-    #   user = User.create
-    #   user.provider = auth.provider
-    #   user.uid = auth.uid
-    #   user.email = auth.info.email
-    #   user.password = Devise.friendly_token[0,20]
-    #
-    #   user.first_name = auth.info.first_name
-    #   user.last_name = auth.info.last_name
-    #   user.headline = auth.info.headline
-    #   user.profile_url = auth.info.urls.public_profile
-    #   user.profile_image = auth.info.image
-    # end
-    # user
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 100.times do |x|
-  User.create(:email => "user#{x}@gmail.com", :password => '12341234')
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  User.create(:email => "#{first_name}.#{last_name}@example.com", :password => '12341234', :first_name => first_name, :last_name => last_name, :headline => "Freelancer", :profile_url => "https://www.linkedin.com/in/#{first_name}#{last_name}", :profile_image => "https://media.licdn.com/mpr/mprx/0_IcAN_IXT2gHer0BMWl-w_eqymV7ktybMdrLI_Wv3l0UvS4KJbA3ZDd3f8NfsAUQvoq1EuaIgoIlx")
 end
