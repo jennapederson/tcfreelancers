9e611d371238ff5fe8e639d4f482928ea969b0f4
Jenna Pederson <jenna@612softwarefoundry.com>
2015-01-20 19:38:53 +0000

    Go to root route when logging in; Only show account cancellation for existing users

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,8 +1,13 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def linkedin
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in @user
 
-    redirect_to edit_user_registration_path
+    if @user.last_sign_in_at.nil?
+        sign_in @user
+        redirect_to edit_user_registration_path({:new => true})
+    else
+        sign_in @user
+        redirect_to root_path
+    end
   end
 end
