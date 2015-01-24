dcfe93c2e50d49bc45bd4e70af31ca49c12cdfd7
Jenna Pederson <jenna@612softwarefoundry.com>
2015-01-24 19:44:02 +0000

    Remove unused tags

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Tcfreelancers
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    ActsAsTaggableOn.remove_unused_tags = true
   end
 end
