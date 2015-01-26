00cf2ed1006a9f30965aade2196e13b0f43fa482
Jenna Pederson <jenna@612softwarefoundry.com>
2015-01-26 02:48:28 +0000

    Remove config to cleanup old specialties - seemed buggy

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module Tcfreelancers
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    ActsAsTaggableOn.remove_unused_tags = true
+    #ActsAsTaggableOn.remove_unused_tags = true
   end
 end
